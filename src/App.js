09d89ddfc1e2cd20c225c0655d9d8ce4e73653e2
Bryson69 <brysonmundia@gmail.com>
2022-01-01 14:18:40 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import logo from './images/logo.svg';
+import buttonIcon from './images/icon-arrow.svg';
+import heroImg from './images/hero-desktop.jpg';
+import './App.css';
+import React, { useState, useEffect } from "react";
+
+
+
+function App() {
+
+  const initialValue = {email: "", };
+  const [formValue, setformValue] = useState(initialValue);
+  const [formError, setformError] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setformValue({ ...formValue, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setformError(validate(formValue));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formError);
+    if (Object.keys(formError).length === 0 && isSubmit) {
+      console.log(formValue);
+    }
+  }, [formError]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    else if (regex.test(values.email)) {
+      errors.email = "Your Email has been saved Successfully";
+    }
+    return errors;
+  };
+
+
+  return (
+    <div className="container">
+      <div className='left-side'>
+        <div className='left'>
+          <img src={logo} className='logo' alt="logo" />
+          <div className='headline-content'>
+            <h2 className='heading'><span className='headline-link'>WE'RE</span> <br /> COMING <br /> SOON</h2>
+            <p className='headline-paragraph'> Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcemments and our launch deals</p>
+          </div>
+          <div> 
+            <form onSubmit={handleSubmit}>
+          <input 
+            type='text'
+            name="email"
+            value={formValue.email}
+            onChange={handleChange}
+            placeholder='Email Address'
+            className='input'
+          />
+          <button type='submit' className='button'> <img src={buttonIcon}  alt="" /> </button>
+          <p className='message'>{formError.email}</p>
+          </form>
+          </div>
+        </div>
+      </div>
+      <div className='right-side'>
+        <img src={heroImg} className='hero-img' alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ style-guide.md
@@ -0,0 +1,35 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+- Desaturated Red: hsl(0, 36%, 70%)ce9797
+- Soft Red: hsl(0, 93%, 68%) F96262
+
+### Neutral
+
+- Dark Grayish Red: hsl(0, 6%, 24%) 413A3A
+
+### Gradients
+
+- Linear, 135deg, from hsl(0, 0%, 100%),FFFFFF to hsl(0, 100%, 98%)FFF5F5
+- Linear, 135deg, from hsl(0, 80%, 86%),f8bfbf to hsl(0, 74%, 74%)EE8C8C
+
+## Typography
+
+### Body Copy
+
+- Font size: 16px
+
+### Font
+
+- Family: [Josefin Sans](https://fonts.google.com/specimen/Josefin+Sans)
+- Weights: 300, 400, 600
